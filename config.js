e230fbb34404f10c42e4e6834d4ac50dec811b30
fcmmendesconsultoria-beep <fcmmendesconsultoria@gmail.com>
2026-05-05 13:44:33 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 // config.js
-// IMPORTANTE: Substitua pelas suas credenciais do Supabase
+
 const SUPABASE_URL = 'https://irgebbwehnaapeveaygj.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlyZ2ViYndlaG5hYXBldmVheWdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MTQ1ODcsImV4cCI6MjA5MzQ5MDU4N30.0wwATTpo0s3JdqM-E1jE7hnYr54fktFP7x4oRGv2VMA';
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// cria só uma vez
+if (!window.supabaseClient) {
+    window.supabaseClient = window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_ANON_KEY
+    );
+}
